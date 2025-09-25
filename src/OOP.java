f98cd7c41c9c16bfa7055a55d3dacb48a7466ce6
mnsengiyumva <nsengiyumvamico@gmail.com>
2025-09-24 22:50:08 -0300

    Shoes class

--- src/OOP.java
+++ src/OOP.java
@@ -10,61 +10,6 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 
 
-class Shoes{
-
-    public String brandName;
-    public String modelNumber;
-    public int productionYear;
-    int brandCost;
-
-
-    public Shoes(String name, String model, int year, int cost){
-        this.brandName = name;
-        this.modelNumber = model;
-        this.productionYear = year;
-        this.brandCost = cost;
-
-    }
-
-    public String getBrandName(){
-        return this.brandName;
-    }
-    public void setBrandName(String newName){
-        this.brandName = newName;
-    }
-
-    public void setProductionYear(int newYear){
-        this.productionYear = newYear;
-    }
-
-    public int getCost(){
-        return this.brandCost;
-    }
-
-    public void writeNamesInFile(){
-        try{
-
-            HashMap<String, String> namesAndModel = new HashMap<String, String>();
-            namesAndModel.put(this.brandName, this.modelNumber);
-            FileWriter myFile = new FileWriter("BrandNames.txt");
-
-            myFile.write("This is a HashMap for "+this.brandName);
-
-            myFile.write(String.valueOf(namesAndModel));
-
-            myFile.close();
-
-        }
-        catch(IOException e){
-            System.out.println("An error has occurred while writing the file");
-            e.printStackTrace();
-        }
-
-
-    }
-
-}
-
 class Adidas extends Shoes{
 
     String shoeName;
