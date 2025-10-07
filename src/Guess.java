4529771a3506f32e3a82c4d99b2180fd4934e126
mnsengiyumva <nsengiyumvamico@gmail.com>
2025-10-06 23:15:20 -0300

    number Guessing game

---
+++ src/Guess.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+
+public class Guess{
+
+    String playerName;
+    int playerAge;
+
+    public Guess(String name, int age){
+
+        this.playerName = name;
+        this.playerAge = age;
+
+    }
+
+    public int getPlayerAge(){
+        return this.playerAge;
+    }
+
+    public String getPlayerName(){
+        return this.playerName;
+    }
+
+    public static void main(String[] args){
+
+
+
+
+    }
+
+
+
+
+}
