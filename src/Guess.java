1faef2fb1159229beebf15d598ef7b991fb194d8
micoupei <mnsengiyumva16594@upei.ca>
2025-10-17 15:49:28 -0300

    Guessing game latest

--- src/Guess.java
+++ src/Guess.java
@@ -9,20 +9,19 @@ public class Guess{
     private String playerName;
     private int playerAge;
     private int computerGuess;
-    public int attempts = 10;
-    private int numberOfPlayers;
+    public int attempts = 3;
 
     private static HashMap<String, Integer> playersScores = new HashMap<>();
 
     private int scores = 10;
 
 
-    public Guess(String name, int age, int computer, int players){
+    public Guess(String name, int age, int computer){
 
         this.playerName = name;
         this.playerAge = age;
         this.computerGuess = computer;
-        this.numberOfPlayers = players;
+
 
     }
 
@@ -42,70 +41,67 @@ public class Guess{
         return computerGuess;
     }
 
+
+
     public void play(){
 
-        while (this.attempts>0 &&numberOfPlayers>0){
-            Scanner currentPlayer = new Scanner(System.in);
+        System.out.print("Who is playing: ");
+        Scanner currentPlayer = new Scanner(System.in);
 
-            System.out.println("Welcome to the game");
-            System.out.print("Who is playing: ");
-            String player = currentPlayer.nextLine();
-            System.out.println("Welcome to the game "+player+" Good luck!");
+        String player = currentPlayer.nextLine();
+
+
+
+        while (attempts>0) {
 
             Scanner userGuess = new Scanner(System.in);
-
             System.out.print(player+" Guess the number between 1 and 100: ");
 
             int guessedNumber = userGuess.nextInt();
 
-            for (int i = 0; i<=numberOfPlayers; i++) {
-
-                numberOfPlayers --;
+            attempts -- ;
 
 
-                if (guessedNumber < getComputerGuess() && attempts > 0) {
-                    System.out.println("Too low. Try again later");
-                    attempts--;
-                    scores --;
+            if (guessedNumber < getComputerGuess() && attempts > 0) {
+                System.out.println("Too low. Try again later");
+                scores --;
 
-                } else if (guessedNumber > getComputerGuess() && attempts > 0) {
+            } else if (guessedNumber > getComputerGuess() && attempts > 0) {
 
-                    System.out.println("Too high. Try again later");
-                    attempts--;
-                    scores --;
+                System.out.println("Too high. Try again later");
+                scores --;
 
-                } else if (guessedNumber == getComputerGuess() && attempts > 0) {
+            } else if (guessedNumber == getComputerGuess() && attempts > 0) {
 
-                    System.out.println("Congratulations You won your guess is: " + guessedNumber + " and computer guess is: " + computerGuess);
-                    System.out.println(player+ "scored: "+scores);
-                    playersScores.put(player, scores);
+                System.out.println("Congratulations You won your guess is: " + guessedNumber + " and computer guess is: " + computerGuess);
+                System.out.println(player+ "scored: "+scores);
+                playersScores.put(player, scores);
 
-                    break;
+                break;
 
-                }
-                if (attempts < 0) {
+            }
+            else{
 
-                    System.out.println("You lost, the number is " + computerGuess + ". You run out of attempts. See you again");
-                    break;
-
-                }
-
-
+                System.out.println("You lost, the number is " + computerGuess + ". You run out of attempts. See you again");
+                break;
 
             }
 
 
+
+
         }
+
 
     }
 
     public static HashMap<String, Integer> getScores() {
-        return playersScores;
+        return Guess.playersScores;
     }
 
     public void winner(){
 
-        ArrayList<Integer> winnersScores = new ArrayList<>(playersScores.values());
+        ArrayList<Integer> winnersScores = new ArrayList<>(Guess.playersScores.values());
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -7,39 +7,42 @@ public class Main{
 //        password1.setPassWord("123458");
 //        password1.setPassWord("123defrtgr");
 
-        Invoice invoice1 = new Invoice("2001022", "Thomas");
-        Invoice invoice2 = new Invoice("20034667", "Peter");
-        Invoice invoice3 = new Invoice("20382kke3", "Peterson");
-        Invoice invoice5 = new Invoice("20010RTH", "Thompson");
-        Invoice invoice6 = new Invoice("20034667", "Lucas");
-        Invoice invoice4= new Invoice("203BBER", "Ceasar");
+//        Invoice invoice1 = new Invoice("2001022", "Thomas");
+//        Invoice invoice2 = new Invoice("20034667", "Peter");
+//        Invoice invoice3 = new Invoice("20382kke3", "Peterson");
+//        Invoice invoice5 = new Invoice("20010RTH", "Thompson");
+//        Invoice invoice6 = new Invoice("20034667", "Lucas");
+//        Invoice invoice4= new Invoice("203BBER", "Ceasar");
+//
+//        invoice2.setCustomerName("Kevin");
+//        invoice1.setCustomerName("MOMO");
+//
+//
+//
+//
+//        invoice1.addItem(5000.0);
+//        invoice2.addItem(200.0);
+//        invoice2.addItem(10002);
+//
+//        if (invoice1.getTotalAmount()>100){
+//            System.out.println(invoice1.getCustomerName()+" You are eligible for a gift card");
+//        }
+//        if (invoice2.getTotalAmount()>100){
+//            System.out.println(invoice2.getCustomerName()+" You are eligible for a gift card");
+//
+//
+//        }
+//
+//
+//
+//        invoice2.printInvoice();
+//        invoice1.printInvoice();
+//
+//        invoice4.printInvoice();
+//        invoice5.printInvoice();
 
-        invoice2.setCustomerName("Kevin");
-        invoice1.setCustomerName("MOMO");
-
-
-
-
-        invoice1.addItem(5000.0);
-        invoice2.addItem(200.0);
-        invoice2.addItem(10002);
-
-        if (invoice1.getTotalAmount()>100){
-            System.out.println(invoice1.getCustomerName()+" You are eligible for a gift card");
-        }
-        if (invoice2.getTotalAmount()>100){
-            System.out.println(invoice2.getCustomerName()+" You are eligible for a gift card");
-
-
-        }
-
-
-
-        invoice2.printInvoice();
-        invoice1.printInvoice();
-
-        invoice4.printInvoice();
-        invoice5.printInvoice();
+        Guess player = new Guess("MICO", 22, 70);
+        player.play();
 
 
 
