80195a7e7a31c31ad16a9998a7c68fb8b27f748d
micoupei <mnsengiyumva16594@upei.ca>
2025-11-04 19:55:25 -0400

    Car project initial

---
+++ src/Car.java
@@ -0,0 +1,74 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Car{
+
+    private String carName;
+    private String carID;
+    private String carBrand;
+    private int productionYear;
+    private String productionLocation;
+    private int carCost;
+
+    private ArrayList<String> allCars;
+    private HashMap<String, Integer> carNamesAndYear;
+    private HashMap<String, Integer> carAndCost;
+
+    private HashMap<String, Integer> luxuryCars;
+
+    public Car(String name, String id, String brand, String location, int year, int cost){
+        this.carName = name;
+        this.carBrand = brand;
+        this.carCost = cost;
+        this.carID = id;
+        this.productionLocation = location;
+        this.productionYear = year;
+
+        this.allCars = new ArrayList<>();
+        this.carNamesAndYear = new HashMap<>();
+        this.carAndCost = new HashMap<>();
+        this.luxuryCars = new HashMap<>();
+
+    }
+
+    public void setCarCost(int newCost){
+        this.carCost = newCost;
+
+    }
+
+    public String getCarNames(){
+        return this.carName;
+    }
+
+    public ArrayList<String> getAllCars(){
+        this.allCars.add(this.carName);
+        return this.allCars;
+    }
+
+    public HashMap<String, Integer> getCarNamesAndYear(){
+        this.carNamesAndYear.put(this.carName, this.productionYear);
+        return carNamesAndYear;
+    }
+
+    /*** The luxury cars are the cars with the cost above $20000, and they are produced al least in 2020
+     *
+     * @return the hashmap of those cars
+     */
+
+    public HashMap<String, Integer> getLuxuryCars(){
+
+        if(this.carCost>20000 && this.productionYear >= 2020){
+            this.luxuryCars.put(this.carName, this.carCost);
+        }
+
+        return this.luxuryCars;
+
+
+    }
+
+
+
+
+}
